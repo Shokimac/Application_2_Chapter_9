b8933b2efb0f13eea9d5e9836ebd00badc75b96e
横溝 翔貴 <shokimac@Shokis-Mac.local>
2020-06-17 22:02:29 +0900

    Mypage_make

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'books#top'
   devise_for :users
-  resources :books
+  resources :books, only: [:new, :create, :index, :show]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
