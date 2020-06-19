f0d3f56cc1692400df352f5507397cd5535f082b
横溝 翔貴 <shokimac@Shokis-Mac.local>
2020-06-19 15:05:05 +0900

    User_show_view_left_side_make

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cae1e34cd97a15a1c931b8e9aff98b34bf9173dd7602c2b5bc786025950e6f7250d5368b65fa8e7190111e613c115d39853b2fdfb2107fd767c6c872485af1db'
+
